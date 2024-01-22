ad43f45d239188e0d87749ffa3df5a81997b9521
GaganDureja <gagandureja675@gmail.com>
2024-01-22 14:48:09 +0530

    all urls on homepage

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.urls import get_resolver
 
 # Create your views here.
+
+
+
+
+
+def home(request):
+    url_patterns = get_resolver().url_patterns
+    html = "<html><body><ul>"
+
+    for pattern in url_patterns:
+        html+= '<a href="{}" target="_blank">{}</a><br>'.format(pattern.pattern, pattern.pattern)
+    html+="</ul></body></html>"
+    print(html)
+
+    return HttpResponse(html)
+
+def test_annotate(request):
+    return HttpResponse("Hi")
